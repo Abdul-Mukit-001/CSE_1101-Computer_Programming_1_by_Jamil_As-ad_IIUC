8968f90275f5a0ee3aa3db015d7cc8274c4eeb7a
Abdul Mukit <amukit001@gmail.com>
2023-07-29 23:57:17 +0600

    Add files via upload

--- bee_13_1012.c
+++ bee_13_1012.c
@@ -1,27 +1,9 @@
 #include<stdio.h>
 int main(){
-    double a,b,c;
-    scanf("%lf %lf %lf",&a,&b,&c);
-    double a_r_t = (1.0/2)*a*c;
-    //a_r_t = the area of the rectangled triangle
-    //a --> base & c --> height
-    printf("TRIANGULO: %.3lf\n",a_r_t);
-    double pi = 3.14159;
-    double a_r_c = pi*c*c;
-    //a_r_c = the area of the radius's circle
-    //c --> radius(r) & pi --> 3.14159
-    printf("CIRCULO: %.3lf\n",a_r_c);
-    double a_t = ((a+b)*c)/2;
-    //a_t = the area of the trapezium
-    //a,b --> base & c --> height
-    printf("TRAPEZIO: %.3lf\n",a_t);
-    double a_s = b*b;
-    //a_s = the area of the square
-    //b(4) --> side
-    printf("QUADRADO: %.3lf\n",a_s);
-    double a_r = a*b;
-    //a_r = the area of the rectangle
-    //a(2),b(2) --> side
-    printf("RETANGULO: %.3lf\n",a_r);
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    int maior_ab = (a+b+abs(a-b))/2;
+    int maior_ab_c = (maior_ab+c+abs(maior_ab-c))/2;
+    printf("%d eh o maior\n",maior_ab_c);
     return 0;
 }
